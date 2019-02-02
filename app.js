b8287192364bd38d66750c45d94b56046f04e0dc
Mostafa-shaban <dashassas@gmail.com>
2019-02-02 06:27:39 +0200

    modify winning score

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ document.querySelector('.btn-hold').addEventListener('click', function(){
 		document.querySelector('#score-'+ activePlayer).textContent = scores[activePlayer];
 		
 		//Check if player won the game 
-		if (scores[activePlayer] >= 10) {
+		if (scores[activePlayer] >= 100) {
 			document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
 			document.querySelector('.player-'+ activePlayer+ '-panel').classList.add('winner');
 			document.querySelector('.player-'+ activePlayer+ '-panel').classList.remove('active');
